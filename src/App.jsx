219556ba13ecfaac2bd084322d390b8c3a48f19b
Param Parikh <132662200+param2412@users.noreply.github.com>
2024-08-28 18:07:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+
+function App() {
+  const [count, setCount] = useState("olive")
+
+  return (
+    <div className='w-full h-screen duration-200 '>
+      style={{backgroundColor: "#000"}}
+      
+    </div>
+  )
+}
+
+export default App
