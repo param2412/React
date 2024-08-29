105db66262bc837c4e470cf3ac6007f3dfeffd84
Param Parikh <132662200+param2412@users.noreply.github.com>
2024-08-29 21:36:01 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [color, setColor] = useState("olive")
 
   return (
-    <div className="w-full h-screen duration-200"
+    <div className="w-screen h-screen duration-200"
     style={{backgroundColor: color}}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
